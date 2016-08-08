dbb27bf4481c47c426862f612a79d2e89cf148cd
Nabil <nmiri127@gmail.com>
2016-08-07 22:27:48 -0230

    Added assimp to include dirs and implemented mesh class

---
+++ ShaderViewer/Mesh.cpp
@@ -0,0 +1,66 @@
+#include "Mesh.h"
+
+/***********************************************************************************/
+Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<GLuint>& indices, const std::vector<Texture>& textures) :
+	m_vertices(vertices), 
+	m_indices(indices), 
+	m_textures(textures) {
+
+	//Construct mesh
+	setupMesh();
+}
+
+/***********************************************************************************/
+Mesh::~Mesh() {
+}
+
+/***********************************************************************************/
+void Mesh::Draw(const Shader& shader) {
+	GLuint diffuseNr = 1;
+	GLuint specularNr = 1;
+
+	for (GLuint i = 0; i < m_textures.size(); ++i) {
+		glActiveTexture(GL_TEXTURE0 + i); // Activate proper texture unit before binding
+										  // Retrieve texture number (the N in diffuse_textureN)
+		
+		std::string name = m_textures[i].type;
+		std::string number = (name == "texture_diffuse") ? std::to_string(diffuseNr++) : std::to_string(specularNr++);
+
+		glUniform1i(shader.GetUniformLoc("material." + name + number), i);
+		glBindTexture(GL_TEXTURE_2D, m_textures[i].id);
+	}
+	glActiveTexture(GL_TEXTURE0);
+
+	// Draw mesh
+	glBindVertexArray(m_vao);
+	glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
+
+/***********************************************************************************/
+void Mesh::setupMesh() {
+
+	glGenVertexArrays(1, &m_vao);
+	glGenBuffers(1, &m_vbo);
+	glGenBuffers(1, &m_ebo);
+
+	glBindVertexArray(m_vao);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(Vertex), &m_vertices.at(0), GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_indices.size() * sizeof(GLuint), &m_indices.at(0), GL_STATIC_DRAW);
+
+	// Vertex Positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+	// Vertex Normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Normal));
+	// Vertex Texture Coords
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, TexCoords));
+
+	glBindVertexArray(0);
+}
---
+++ ShaderViewer/Mesh.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <GL\glew.h>
+#include <glm\glm.hpp>
+
+#include "Shader.h"
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+struct Texture {
+	GLuint id;
+	std::string type;
+};
+
+class Mesh {
+public:
+	Mesh(const std::vector<Vertex>& vertices, const std::vector<GLuint>& indices, const std::vector<Texture>& textures);
+	~Mesh();
+	
+	void Draw(const Shader& shader);
+
+private:
+	void setupMesh();
+
+	GLuint m_vao, m_vbo, m_ebo;
+	std::vector<Vertex> m_vertices;
+	std::vector<GLuint> m_indices;
+	std::vector<Texture> m_textures;
+};
+
